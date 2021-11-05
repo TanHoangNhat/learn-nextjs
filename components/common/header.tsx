f58255c36d15eb3b26f2636749e18d7c2ac19140
TanHN <tanhn.3004@gmail.com>
2021-11-05 15:36:16 +0700

    SSG: - Fetch data on client side

---
+++ components/common/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export interface HeaderProps {}
+
+export default function Header(props: HeaderProps) {
+  console.log('render Header')
+  return <div>Header</div>
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import Header from '@/components/common/header'
+import { useRouter } from 'next/dist/client/router'
+import React, { useState, useEffect } from 'react'
+// import dynamic from 'next/dynamic'
 
-export interface AboutProps {
-}
+// const Header = dynamic(() => import('@/components/common/header'), { ssr: true })
 
-export default function About (props: AboutProps) {
+export interface AboutProps {}
+
+export default function About(props: AboutProps) {
+  const router = useRouter()
+
+  const [postList, setPostList] = useState([])
+  const page = router.query?.page
+  useEffect(() => {
+    if (!page) return
+    ;(async () => {
+      const response = await fetch(`https://js-post-api.herokuapp.com/api/posts?_page=${page}`)
+      const data = await response.json()
+      setPostList(data.data)
+    })()
+  }, [page])
+
+  function handleNextClick() {
+    router.push(
+      {
+        pathname: '/about',
+        query: {
+          page: (Number(page) || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    )
+  }
+
   return (
     <div>
-      About
+      <h1>About</h1>
+
+      <Header />
+
+      <ul>
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleNextClick}>Next Page</button>
     </div>
-  );
+  )
 }
